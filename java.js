492efc1a813da638a1ebec02db733a0a4f032ae3
j-eppe <jesper.blom68@gmail.com>
2022-05-19 22:28:52 +0200

    fem

--- java.js
+++ java.js
@@ -1,12 +1,3 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'the-sneaker-database.p.rapidapi.com',
-		'X-RapidAPI-Key': '6a1f5c327amshae901d139c2c676p182734jsnffaf16445d34'
-	}
-};
-
-fetch('https://the-sneaker-database.p.rapidapi.com/sneakers?limit=20', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function displayDate() {
+	document.getElementById("demo").innerHTML = Date();
+  }
